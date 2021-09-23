898c2ae54df2db40253866c3e44d43f287a62b85
Ronaldo Queiroz <ronquesil@gmail.com>
2021-09-22 23:56:31 -0300

    Práticas da aula 07
    Testes dos exercícios

---
+++ Testes/aula07a.py
@@ -0,0 +1,6 @@
+nome = input('Qual é seu nome? ')
+print('Prazer em te conhecer {}!'.format(nome))
+#print('Prazer em te conhecer {:20}!'.format(nome)) //Print com 20 caracteres
+#print('Prazer em te conhecer {:>20}!'.format(nome)) //Print alinhado a direita
+#print('Prazer em te conhecer {:^20}!'.format(nome)) //Print alinhado no centro
+#print('Prazer em te conhecer {:=^20}!'.format(nome)) //Print alinhado no centro com caractere
