acf3b7cb4a07e34ffd34db36f0f6a2843d405ad1
Ronaldo Queiroz <ronquesil@gmail.com>
2021-09-23 20:44:42 -0300

    Create Desafio05.py
    Desafio 05 - Antecessor e Sucessor. Usando três variáveis e uma variável.

---
+++ Desafios/Desafio05.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um número: '))
+#a = int(n1-1)
+#s = int(n1+1)
+#print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n1, a, s)) # usando três variáveis
+print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n1, (n1-1), (n1+1))) # usando uma variável
